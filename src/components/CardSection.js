afc1824881a47a23ed49a69ca0c14a85dd171e25
Tor Schandall <tor@schandall.net>
2017-07-03 22:38:51 -0400

    Basic component work

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const CardSection = () => {
+const CardSection = (props) => {
   return (
-    <View>
-
+    <View style={styles.containerStyle}>
+      {props.children}
     </View>
   );
+};
 
+const styles = {
+  containerStyle: {
+    borderBottomWidth: 0,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
+  }
 };
 
 export default CardSection;
